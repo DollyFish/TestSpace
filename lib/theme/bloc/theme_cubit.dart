104117fea35d15c9302172a3f340be6dc656c329
DollyFish <dolhinta@kkumail.com>
2023-09-18 16:05:44 +0700

    set font family

--- lib/theme/bloc/theme_cubit.dart
+++ lib/theme/bloc/theme_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 
 class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit() : super(_lightTheme);
@@ -7,13 +8,13 @@ class ThemeCubit extends Cubit<ThemeData> {
   static final _lightTheme = ThemeData(
     brightness: Brightness.light,
     useMaterial3: true,
-    // fontFamily: FlutterLocalization.instance.fontFamily,
+    fontFamily: FlutterLocalization.instance.fontFamily,
   );
 
   static final _darkTheme = ThemeData(
     brightness: Brightness.dark,
     useMaterial3: true,
-    // fontFamily: FlutterLocalization.instance.fontFamily,
+    fontFamily: FlutterLocalization.instance.fontFamily,
   );
 
   void toggleTheme() {
