03eccd0c39d7a21dea41803bf7c4aaa6dcea5d80
DollyFish <dolhinta@kkumail.com>
2023-09-18 16:05:19 +0700

    remove unused code

--- lib/theme/bloc/theme_cubit.dart
+++ lib/theme/bloc/theme_cubit.dart
@@ -5,18 +5,12 @@ class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit() : super(_lightTheme);
 
   static final _lightTheme = ThemeData(
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      foregroundColor: Colors.white,
-    ),
     brightness: Brightness.light,
     useMaterial3: true,
     // fontFamily: FlutterLocalization.instance.fontFamily,
   );
 
   static final _darkTheme = ThemeData(
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      foregroundColor: Colors.black,
-    ),
     brightness: Brightness.dark,
     useMaterial3: true,
     // fontFamily: FlutterLocalization.instance.fontFamily,
